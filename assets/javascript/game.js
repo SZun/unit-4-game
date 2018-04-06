398a324c2cb7471790255056d872d7dde1bd4961
Samuel Zun <Zun@samuels-mbp-2.attlocal.net>
2018-04-06 04:38:22 -0500

    commit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -13,15 +13,15 @@ var losses = 0;
 var userNum = 0;
 
 //onKeyUp()
-document.onkeyup = function(userInput) {
-    var userNum = userInput.key;
+document.onclick = function(userInput) {
+    var userNum = userInput.onclick 
 
 // jQuery .onClick events
 
 //If Red Crystal Clicked
 $("#redCrystal").on("click", function() {
     //Add Random number for red to 
-    userNum + redNumGen;
+    userNum += redNumGen;
     //If userNum is equal to target
     if (userNum === trgtGen) {
         //alert win 
@@ -46,14 +46,14 @@ $("#redCrystal").on("click", function() {
     }
     //Add new userNum to users total
     else {
-        $("totalBox").push(+ userNum);
+        $("totalBox").push(userNum);
     }
-})
+});
 //If Red Crystal Clicked
 $("#blueCrystal").on("click", function() {
     //Functions
     //Add Random number for blue to userNum
-    userNum + blueNumGen;
+    userNum += blueNumGen;
     //If userNum is equal to target
     if (userNum === trgtGen) {
         //alert win 
@@ -78,14 +78,14 @@ $("#blueCrystal").on("click", function() {
     }
     //Add new userNum to users total
     else {
-        $("totalBox").push(+ userNum);
+        $("totalBox").push(userNum);
     }
-})
+});
 //If Red Crystal Clicked
 $("#greenCrystal").on("click", function() {
     //Functions
     //Add Random number for green to userNum
-    userNum + greenNumGen;
+    userNum += greenNumGen;
     //If userNum is equal to target
     if (userNum === trgtGen) {
         //alert win 
@@ -110,13 +110,13 @@ $("#greenCrystal").on("click", function() {
     }
     //Add new userNum to users total
     else {
-        $("totalBox").push(+ userNum);
+        $("totalBox").push(userNum);
     }
-})
+});
 //If Red Crystal Clicked
 $("#yellowCrystal").on("click", function() {
     //Add Random number for yellow to userNum
-    userNum + yellowNumGen;
+    userNum += yellowNumGen;
     //If userNum is equal to target
     if (userNum === trgtGen) {
         //alert win 
@@ -141,9 +141,20 @@ $("#yellowCrystal").on("click", function() {
     }
     //Add new userNum to users total
     else {
-        $("totalBox").push(+ userNum);
+        $("totalBox").push(userNum);
     }
-})
+});
 
-};
 //html
+$("#number").html(trgtGen); 
+// $("#number").text(trgtGen);
+// $("#number").append(trgtGen);
+$("#totalBox").html(userNum);
+$("#scoreLossText").html("Losses: " + losses);
+$("#scoreWinText").html("Wins: " + wins);
+$("#redCrystal").html(redNumGen);
+$("#blueCrystal").html(blueNumGen);
+$("#greenCrystal").html(greenNumGen);
+$("#yellowCrystal").html(yellowNumGen);
+};
+
